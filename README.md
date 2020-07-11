0b284ee9fd853523bd06a058eeec9fa52647c02b
ksy <ksy8230@hanmail.net>
2020-07-11 19:52:06 +0900

    mutation 사용해서 데이터 변형해보기

--- README.md
+++ README.md
@@ -9,11 +9,11 @@
 - resolvers 파일 : query를 resolve 한다.
 (데이터베이스에 query를 던지고 이를 resolver 파일에 적힌대로 해석하게 만든다)
 
-3. 단어들
+3. playground에서 사용하는 명령어들
 - query : 정보를 받을 때 사용
 - mutation : 데이터 변형을 의미
 
-graphql은 설명과 서버에 이러한 유형들을 정의한다.
+graphql은 위와 같은 유형들을 정의한다.
 (어떤 mutation, 어떤 query들을 우리가 가졌는지...)
 
 - playground : postman 같은 것으로 데이터베이스를 테스틓볼수 있다
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,13 +1,14 @@
  
- const mollang = {
-     name : "mollang",
-     age : 30,
-     gender: 'male'
- };
+import {getMovies, getById, addMovie, deleteMovie} from './db'
 
  const resolvers = {
     Query: {
-        person: () => mollang
+        movies: () => getMovies(),
+        movie:(_, {id}) => getById(id)
+    },
+    Mutation: {
+        addMovie: (_, {name, score}) => addMovie(name, score),
+        deleteMovie: (_, {id}) => deleteMovie(id)
     }
 }
 
